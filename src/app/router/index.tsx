ce7133513e16e8040e45ba4dbaecc1efc612f69a
krevetka87 <bonanast4sia@yandex.ru>
2023-09-06 20:02:11 +0300

    feat: add router

---
+++ src/app/router/index.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from 'react-router-dom';
+import RootLayout from '@/app/RootLayout';
+import HomeView from '@/views/HomeView';
+import NotFoundView from '@/views/NotFoundView';
+import BookView from '@/views/BookView';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<HomeView />} />
+      <Route path="book/:id" element={<BookView />} />
+      <Route path="page-not-found" element={<NotFoundView />} />
+      <Route path="*" element={<Navigate to="page-not-found" />} />
+    </Route>
+  )
+);
+
+export default router;
